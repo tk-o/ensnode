23a00c1a7dd330b5d71a3974e381805a036a1215
Sebastian Banaś <96051496+BanaSeba@users.noreply.github.com>
2025-03-24 16:38:44 +0100

    Feat/dispose of old alpha uri (#440)

--- apps/ensadmin/src/lib/env.ts
+++ apps/ensadmin/src/lib/env.ts
@@ -100,7 +100,7 @@ export function selectedEnsNodeUrl(params: URLSearchParams): string {
   return new URL(params.get("ensnode") || preferredEnsNodeUrl()).toString();
 }
 
-const PREFERRED_ENSNODE_URL = "https://alpha.ensnode.io";
+const PREFERRED_ENSNODE_URL = "https://api.alpha.ensnode.io";
 
 export function preferredEnsNodeUrl(): string {
   const envVarName = "NEXT_PUBLIC_PREFERRED_ENSNODE_URL";
