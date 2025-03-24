3d4572b385b5df6066501aca9f7aaac116808279
Tomek Kopacki <tomasz@kopacki.net>
2025-03-24 13:26:51 +0100

    fix(ensadmin): use dynamic metadata (#433)
    Co-authored-by: lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>

--- apps/ensadmin/src/app/layout.tsx
+++ apps/ensadmin/src/app/layout.tsx
@@ -21,32 +21,34 @@ const siteName = "ENSAdmin";
 const title = "ENSAdmin";
 const description = "Explore the ENS Protocol like never before";
 
-export const metadata: Metadata = {
-  title: title,
-  description: description,
-  metadataBase: new URL(ensAdminPublicUrl()),
-  openGraph: {
-    title: {
-      template: `${siteName} - %s`,
-      default: title,
-    },
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: title,
     description: description,
-    url: "/",
-    type: "website",
-    siteName: siteName,
-    images: ["/opengraph-image.png"],
-  },
-  twitter: {
-    title: {
-      template: `${siteName} - %s`,
-      default: title,
+    metadataBase: ensAdminPublicUrl(),
+    openGraph: {
+      title: {
+        template: `${siteName} - %s`,
+        default: title,
+      },
+      description: description,
+      url: "/",
+      type: "website",
+      siteName: siteName,
+      images: ["/opengraph-image.png"],
     },
-    card: "summary_large_image",
-    site: "@NamehashLabs",
-    creator: "@NamehashLabs",
-    images: ["/twitter-image.png"],
-  },
-};
+    twitter: {
+      title: {
+        template: `${siteName} - %s`,
+        default: title,
+      },
+      card: "summary_large_image",
+      site: "@NamehashLabs",
+      creator: "@NamehashLabs",
+      images: ["/twitter-image.png"],
+    },
+  };
+}
 
 export default function Layout({
   children,
--- apps/ensadmin/src/lib/env.ts
+++ apps/ensadmin/src/lib/env.ts
@@ -1,21 +1,99 @@
 /**
  * Get ENSAdmin service public URL.
+ *
+ * Note: a Vercel fallback URL will be used if application runs on Vercel platform.
+ * If the Vercel fallback URL cannot be applied, then default URL will be used.
+ *
+ * @returns application public URL for ENSAdmin
+ * @throws when Vercel platform was detected but could not determine application hostname
  */
-export function ensAdminPublicUrl() {
+export function ensAdminPublicUrl(): URL {
   const envVarName = "ENSADMIN_PUBLIC_URL";
   const envVarValue = process.env[envVarName];
 
   if (!envVarValue) {
-    throw new Error(`Required "${envVarName}" value was not set`);
+    if (isAppOnVercelPlatform()) {
+      // build public URL using the Vercel-specific way
+      return getVercelAppPublicUrl();
+    }
+
+    // otherwise, use default public URL
+    return defaultEnsAdminPublicUrl();
   }
 
   try {
-    return parseUrl(envVarValue).toString();
+    return parseUrl(envVarValue);
   } catch (error) {
     console.error(error);
 
     throw new Error(`Invalid ${envVarName} value "${envVarValue}". It should be a valid URL.`);
   }
+}
+
+/** Build a default public URL for ENSAdmin */
+function defaultEnsAdminPublicUrl(): URL {
+  let applicationPort: number;
+
+  try {
+    applicationPort = parseApplicationPort(process.env.PORT);
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "Unknown error";
+
+    console.error(`Error building default public URL for ENSAdmin: ${errorMessage}`);
+
+    applicationPort = 4173;
+  }
+
+  return new URL(`http://localhost:${applicationPort}`);
+}
+
+function parseApplicationPort(rawValue?: string): number {
+  if (!rawValue) {
+    throw new Error("Expected value not set");
+  }
+
+  const parsedValue = parseInt(rawValue, 10);
+
+  if (Number.isNaN(parsedValue)) {
+    throw new Error(`'${rawValue}' is not a number`);
+  }
+
+  if (parsedValue <= 0) {
+    throw new Error(`'${rawValue}' is not a natural number`);
+  }
+
+  return parsedValue;
+}
+
+/**
+ * Tells if the application runs on Vercel.
+ */
+function isAppOnVercelPlatform(): boolean {
+  return process.env.VERCEL === "1";
+}
+
+/**
+ * Builds a public URL of the app assuming it runs on Vercel.
+ * @returns public URL
+ * @throws when application hostname could not be determined based on `VERCEL_*` env vars
+ */
+function getVercelAppPublicUrl(): URL {
+  const vercelEnv = process.env.VERCEL_ENV;
+  let vercelAppHostname: string | undefined;
+
+  switch (vercelEnv) {
+    case "production":
+      vercelAppHostname = process.env.VERCEL_PROJECT_PRODUCTION_URL;
+    case "development":
+    case "preview":
+      vercelAppHostname = process.env.VERCEL_BRANCH_URL || process.env.VERCEL_URL;
+  }
+
+  if (!vercelAppHostname) {
+    throw new Error(`Could not extract Vercel hostname for Vercel env "${vercelEnv}"`);
+  }
+
+  return new URL(`https://${vercelAppHostname}`);
 }
 
 export function selectedEnsNodeUrl(params: URLSearchParams): string {
