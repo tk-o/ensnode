ba63b4d3043355b204ce4ef6ce0bd227d8351da6
shrugs <mattgcondon@gmail.com>
2025-04-05 07:25:08 -0500

    fix: snake_case db column names for intersection tables (#495)

--- packages/ponder-subgraph-api/src/graphql.ts
+++ packages/ponder-subgraph-api/src/graphql.ts
@@ -73,6 +73,7 @@ import {
   relations,
   sql,
 } from "drizzle-orm";
+import { toSnakeCase } from "drizzle-orm/casing";
 import {
   type PgColumnBuilderBase,
   PgDialect,
@@ -1074,11 +1075,13 @@ function buildUnionAllQuery(
     // build select object with nulls for missing columns, manually casting them to the correct type
     const selectAllColumnsIncludingNulls = allColumnNames.reduce((memo, columnName) => {
       const column = allColumns[columnName]!;
+      const dbColumnName = table.columns[columnName]
+        ? toSnakeCase(table.columns[columnName].name)
+        : "NULL";
+
       return {
         ...memo,
-        [columnName]: sql
-          .raw(`${table.columns[columnName]?.name ?? "NULL"}::${column.getSQLType()}`)
-          .as(column.name),
+        [columnName]: sql.raw(`${dbColumnName}::${column.getSQLType()}`).as(column.name),
       };
     }, {});
 
