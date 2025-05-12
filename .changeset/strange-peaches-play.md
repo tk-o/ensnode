53a80a1893abaa6ad6f30b945c674c41a1f17399
Daniel Starns <danielstarns@hotmail.com>
2025-05-12 18:28:00 +0700

    feat(ensadmin): update gqlpt to alpha.33 (#709)
    Replace copy-pasted AI query generator functionality with its upstream dependency counter-part.
    
    Co-authored-by: Tomek Kopacki <tomasz@kopacki.net>

---
+++ .changeset/strange-peaches-play.md
@@ -0,0 +1,5 @@
+---
+"ensadmin": patch
+---
+
+Replace copy-pasted AI query generator functionality with its upstream dependency counter-part.
--- apps/ensadmin/src/app/gql/api/route.ts
+++ apps/ensadmin/src/app/gql/api/route.ts
@@ -1,8 +1,8 @@
-import Anthropic, { type ClientOptions } from "@anthropic-ai/sdk";
-import { Adapter, type AdapterResponse } from "@gqlpt/adapter-base";
+import { AdapterAnthropic } from "@gqlpt/adapter-anthropic";
 import { GQLPTClient } from "gqlpt";
 import { type NextRequest } from "next/server";
 import { ensSubgraphSchemaGql } from "./ens-subgraph-gql-schema";
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const maybePrompt = requestUrl.searchParams.get("prompt");
@@ -123,6 +123,9 @@ async function getQueryGeneratorClient(
         systemPrompt: `${SYSTEM_PROMPT}\n\n${ensSubgraphSchemaGql}`,
         maxTokensPerMessage: MAX_TOKENS_PER_MESSAGE,
         temperature: TEMPERATURE,
+        cacheControl: {
+          type: "ephemeral",
+        },
       }),
     });
 
@@ -196,139 +199,7 @@ class GenerateQueryDto {
   }
 }
 
-interface AdapterAnthropicOptions extends ClientOptions {
-  /** The Anthropic model to use */
-  model: Model;
-
-  /** The system prompt to use */
-  systemPrompt: string;
-
-  /** The max tokens to use per message */
-  maxTokensPerMessage: number;
-
-  /** The temperature to use */
-  temperature: number;
-}
-
 enum Model {
   Claude35Sonnet = "claude-3-5-sonnet-20241022",
   Claude37Sonnet = "claude-3-7-sonnet-20250219",
-}
-
-/**
- * Adapter for Anthropic with selectable model and system prompt.
- *
- * Based on https://github.com/rocket-connect/gqlpt/blob/18af9c9/packages/adapter-anthropic/src/index.ts
- */
-class AdapterAnthropic extends Adapter {
-  /** Anthropic client */
-  private anthropic: Anthropic;
-
-  /** The Anthropic model to use */
-  private model: Model;
-
-  /** The system prompt to use */
-  private systemPrompt: string;
-
-  /** The max tokens to use per message */
-  private maxTokensPerMessage: number;
-
-  /** The temperature to use */
-  private temperature: number;
-
-  private messageHistory: Map<string, Array<Anthropic.MessageParam>> = new Map();
-
-  constructor(options: AdapterAnthropicOptions) {
-    super();
-    this.anthropic = new Anthropic(options);
-    this.model = options.model;
-    this.systemPrompt = options.systemPrompt;
-    this.maxTokensPerMessage = options.maxTokensPerMessage;
-    this.temperature = options.temperature;
-  }
-
-  /**
-   * Connect to Anthropic
-   *
-   * Based on https://github.com/rocket-connect/gqlpt/blob/18af9c9/packages/adapter-anthropic/src/index.ts#L18-L30
-   */
-  async connect() {
-    const response = await this.anthropic.messages.create({
-      system:
-        "You are to test the connection to the Anthropic API. Respond with 'Pong' when you see 'Ping'.",
-      messages: [{ role: "user", content: "Ping" }],
-      model: this.model,
-      max_tokens: this.maxTokensPerMessage,
-    });
-
-    if ((response.content[0] as any).text !== "Pong") {
-      throw new Error("Cannot connect to Anthropic");
-    }
-  }
-
-  /**
-   * Send a text message to Anthropic
-   *
-   * Based on https://github.com/rocket-connect/gqlpt/blob/18af9c9/packages/adapter-anthropic/src/index.ts#L32-L61
-   */
-  async sendText(text: string, conversationId?: string): Promise<AdapterResponse> {
-    let beforeText: string | undefined;
-    const splitPhrase = "And this plain text query:";
-
-    let messages: Array<Anthropic.MessageParam>;
-
-    if (text.includes(splitPhrase)) {
-      const splitIndex = text.indexOf(splitPhrase);
-      beforeText = text.substring(0, splitIndex);
-      text = text.substring(splitIndex);
-
-      messages = [
-        {
-          role: "user",
-          content: [
-            {
-              type: "text",
-              text: beforeText,
-              cache_control: { type: "ephemeral" },
-            },
-            {
-              type: "text",
-              text: text,
-            },
-          ],
-        },
-      ];
-    } else {
-      messages = [{ role: "user", content: text }];
-    }
-
-    if (conversationId && this.messageHistory.has(conversationId)) {
-      messages = [...this.messageHistory.get(conversationId)!, ...messages];
-    }
-
-    const response = await this.anthropic.messages.create({
-      // add system prompt if it was provided
-      system: this.systemPrompt,
-      messages,
-      // use the selected model
-      model: this.model,
-      // set a default max tokens
-      max_tokens: 1024,
-      temperature: this.temperature,
-    });
-
-    const content = (response.content[0] as any).text;
-    const newId = response.id;
-
-    this.messageHistory.set(newId, [
-      ...(conversationId ? this.messageHistory.get(conversationId) || [] : []),
-      { role: "user" as const, content: text },
-      { role: "assistant" as const, content },
-    ]);
-
-    return {
-      content,
-      conversationId: newId,
-    };
-  }
 }
