2b262f5b66112fb8232596174f1855eea7f26181
Jędrzej Ziebura <ziebura.jedrzej@gmail.com>
2025-05-16 14:54:55 +0200

    chore: ensnode.io social icons and search keyboard shortcut refinements (#716)

---
+++ docs/ensnode.io/src/components/atoms/GenericTooltip.tsx
@@ -0,0 +1,92 @@
+import { Float } from "@headlessui-float/react";
+import { Popover } from "@headlessui/react";
+import cc from "classcat";
+import React, { useState } from "react";
+
+type Props = {
+  trigger: React.ReactNode;
+  children: React.ReactNode;
+  placement?: TooltipPlacement;
+  maxTooltipWidth?: number;
+  className?: string;
+  withDelay?: boolean;
+};
+
+export const DEFAULT_MAX_TOOLTIP_WIDTH = 400;
+
+export const TooltipPlacement = {
+  Top: "top",
+  Right: "right",
+  Bottom: "bottom",
+  Left: "left",
+} as const;
+
+export type TooltipPlacement = (typeof TooltipPlacement)[keyof typeof TooltipPlacement];
+
+export const DEFAULT_TOOLTIP_PLACEMENT = TooltipPlacement.Top;
+
+export default function GenericTooltip({
+  trigger,
+  children,
+  placement = DEFAULT_TOOLTIP_PLACEMENT,
+  maxTooltipWidth = DEFAULT_MAX_TOOLTIP_WIDTH,
+  className,
+  withDelay = false,
+  /*
+                              Props are applied to the Float component,
+                              which is a wrapper for the tooltip "children".
+                            */
+  ...props
+}: Props) {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Popover className={cc(["flex items-center z-[999]", className])}>
+      <Float
+        show={open}
+        placement={placement} // our `TooltipPlacement` values are a subset of headlessui's `Placement` values
+        offset={15}
+        shift={6}
+        flip={10}
+        arrow
+        // portal // removed due to Astro-related render issues
+        enter={cc([
+          "transition duration-300 ease-out",
+          {
+            "delay-300": withDelay,
+          },
+        ])}
+        enterFrom="opacity-0 -translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="transition duration-200 ease-in delay-0"
+        leaveFrom="opacity-100 translate-y-0"
+        leaveTo="opacity-0 -translate-y-1"
+        {...props}
+      >
+        <Popover.Group
+          onClick={handleOpen}
+          onMouseEnter={handleOpen}
+          onMouseLeave={handleClose}
+          className="cursor-auto"
+        >
+          {trigger}
+        </Popover.Group>
+
+        <Popover.Panel
+          onMouseEnter={handleOpen}
+          onMouseLeave={handleClose}
+          style={{ maxWidth: maxTooltipWidth }}
+          className="rounded-md bg-black focus:outline-none"
+        >
+          <Float.Arrow className="absolute h-5 w-5 rotate-45 bg-black rounded-b" />
+          <div className="relative h-full rounded-md text-sm font-medium text-white p-2">
+            {children}
+          </div>
+        </Popover.Panel>
+      </Float>
+    </Popover>
+  );
+}
---
+++ docs/ensnode.io/src/components/atoms/SearchKeyboardShortcut.tsx
@@ -0,0 +1,31 @@
+import GenericTooltip from "@workspace/docs/ensnode.io/src/components/atoms/GenericTooltip.tsx";
+import { Balancer } from "react-wrap-balancer";
+
+export default function SearchKeyboardShortcut() {
+  const InfoIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      class="size-5 cursor-auto hover:fill-gray-100 hover:stroke-gray-500 transition-all duration-200"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+      />
+    </svg>
+  );
+
+  return (
+    <div className="p-0">
+      <GenericTooltip trigger={InfoIcon}>
+        <Balancer as="p" className="text-center w-fit max-w-[186px]">
+          Start a search by pressing Ctrl+K on your keyboard
+        </Balancer>
+      </GenericTooltip>
+    </div>
+  );
+}
