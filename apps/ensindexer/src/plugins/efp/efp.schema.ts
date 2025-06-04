114021e601b75bd6765dbd6b8b5734de8ae3ffaf
Tomasz Kopacki <tomasz@kopacki.net>
2025-06-04 12:15:31 +0200

    docs(ensindexer): efp plugin schema docs update

--- apps/ensindexer/src/plugins/efp/efp.schema.ts
+++ apps/ensindexer/src/plugins/efp/efp.schema.ts
@@ -7,9 +7,15 @@ import { onchainTable } from "ponder";
 // NOTE: this schema type is based on information from
 // https://docs.efp.app/production/interpreting-state/
 export const efpListStorageLocation = onchainTable("efp_list_storage_location", (p) => ({
-  // list token ID —
+  // List ID
   tokenId: p.bigint().primaryKey(),
+
+  // EVM chain ID of the chain where the list is stored
   chainId: p.bigint().notNull(),
+
+  // EVM address of the contract where the list is stored
   listRecordsAddress: p.hex().notNull(),
+
+  // Specifies the storage slot of the list within the contract
   slot: p.bigint().notNull(),
 }));
