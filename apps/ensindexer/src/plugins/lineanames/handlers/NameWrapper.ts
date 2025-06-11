a41cc26bbef49768f398780d67e4caeca7b22fb1
Nischal Sharma <nischal@web3labs.com>
2025-06-11 17:40:20 +0530

    feat(ensindexer): added Base Sepolia and Linea Sepolia testnets (#772)
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>
    Co-authored-by: Tomasz Kopacki <tomasz@kopacki.net>

--- apps/ensindexer/src/plugins/lineanames/handlers/NameWrapper.ts
+++ apps/ensindexer/src/plugins/lineanames/handlers/NameWrapper.ts
@@ -1,10 +1,15 @@
 import { ponder } from "ponder:registry";
 
+import config from "@/config";
 import { makeNameWrapperHandlers } from "@/handlers/NameWrapper";
 import { ENSIndexerPluginHandlerArgs } from "@/lib/plugin-helpers";
 import { PluginName } from "@ensnode/ensnode-sdk";
+import { getRegistrarManagedName } from "../lib/registrar-helpers";
 
-export default function ({ namespace }: ENSIndexerPluginHandlerArgs<PluginName.Lineanames>) {
+export default function ({
+  pluginName,
+  namespace,
+}: ENSIndexerPluginHandlerArgs<PluginName.Lineanames>) {
   const {
     handleNameWrapped,
     handleNameUnwrapped,
@@ -13,8 +18,9 @@ export default function ({ namespace }: ENSIndexerPluginHandlerArgs<PluginName.L
     handleTransferSingle,
     handleTransferBatch,
   } = makeNameWrapperHandlers({
-    // the shared Registrar handlers in this plugin index direct subnames of '.linea.eth'
-    registrarManagedName: "linea.eth",
+    // the shared Registrar handlers in this plugin index direct subnames of
+    // the name returned from `getRegistrarManagedName` function call
+    registrarManagedName: getRegistrarManagedName(config.ensDeploymentChain, pluginName),
   });
 
   ponder.on(namespace("NameWrapper:NameWrapped"), handleNameWrapped);
