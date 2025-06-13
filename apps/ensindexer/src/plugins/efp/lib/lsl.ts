1d91624b68defbb512e2bea10b78a827c8560ed6
Tomasz Kopacki <tomasz@kopacki.net>
2025-06-13 12:23:07 +0200

    reorder definitions within `lsl.ts` file
    First define elements that will be needed down the file

--- apps/ensindexer/src/plugins/efp/lib/lsl.ts
+++ apps/ensindexer/src/plugins/efp/lib/lsl.ts
@@ -9,60 +9,91 @@ import { getAddress } from "viem/utils";
 import { prettifyError, z } from "zod/v4";
 
 /**
+ * Enum defining recognized List Storage Location Types
+ *
+ * Based on documentation at:
+ * https://docs.efp.app/design/list-storage-location/#location-types
+ */
+export enum ListStorageLocationType {
+  /**
+   * EVMContract Data List Storage Location Type encoding:
+   * 32-byte chain ID + 20-byte contract address + 32-byte slot
+   */
+  EVMContract = 1,
+}
+
+/**
+ * Enum defining recognized List Storage Location Versions
+ *
+ * Based on documentation at:
+ * https://docs.efp.app/design/list-storage-location/#serialization
+ */
+export enum ListStorageLocationVersion {
+  V1 = 1,
+}
+
+/**
+ * Base List Storage Location
+ */
+interface BaseListStorageLocation<
+  LSLVersion extends ListStorageLocationVersion,
+  LSLType extends ListStorageLocationType,
+> {
+  /**
+   * The version of the List Storage Location.
+   *
+   * This is used to ensure compatibility and facilitate future upgrades.
+   */
+  version: LSLVersion;
+
+  /**
+   * The type of the List Storage Location.
+   *
+   * This identifies the kind of data the data field contains.
+   */
+  type: LSLType;
+}
+
+/**
+ * List Storage Location Contract
+ *
+ * Describes data model for the EVM contract Location Type as
+ * a specialized version of BaseListStorageLocation interface,
+ * where the location type is always 1, and, for now, the version is always 1.
+ *
+ * Documented based on https://docs.efp.app/design/list-storage-location/
+ */
+export interface ListStorageLocationContract
+  extends BaseListStorageLocation<
+    ListStorageLocationVersion.V1,
+    ListStorageLocationType.EVMContract
+  > {
+  /**
+   * EVM chain ID of the chain where the EFP list records are stored.
+   */
+  chainId: bigint;
+
+  /**
+   * EVM address of the contract on chainId where the EFP list records are stored.
+   */
+  listRecordsAddress: Address;
+
+  /**
+   * The 32-byte value that specifies the storage slot of the EFP list records within the listRecordsAddress contract.
+   * This disambiguates multiple lists stored within the same contract and
+   * de-couples it from the EFP List NFT token id which is stored on Ethereum and
+   * inaccessible on L2s.
+   */
+  slot: bigint;
+}
+
+/**
  * An encoded List Storage Location is a bytes array with the following structure:
  * - `version`: A uint8 representing the version of the List Storage Location. This is used to ensure compatibility and facilitate future upgrades.
  * - `type`: A uint8 indicating the type of list storage location. This identifies the kind of data the data field contains..
  * - `data:` A bytes array containing the actual data of the list storage location. The structure of this data depends on the location type.
  */
 type EncodedLsl = string;
-
-// NOTE: based on code from https://github.com/ethereumfollowprotocol/onchain/blob/f3c970e/src/efp.ts#L95-L123
-/**
- * Decodes an EncodedLsl into a ListStorageLocationContract.
- *
- * @param {ENSDeploymentChain} ensDeploymentChain - The ENS Deployment Chain
- * @param {EncodedLsl} encodedLsl - The encoded List Storage Location string to parse.
- * @returns A decoded {@link ListStorageLocationContract} object.
- * @throws An error if parsing could not be completed successfully.
- */
-export function decodeListStorageLocationContract(
-  ensDeploymentChain: ENSDeploymentChain,
-  encodedLsl: EncodedLsl,
-): ListStorageLocationContract {
-  const slicedLslContract = sliceEncodedLslContract(encodedLsl);
-  const efpDeploymentChainIds = getEFPDeploymentChainIds(ensDeploymentChain);
-  const efpLslContractSchema = createEfpLslContractSchema(efpDeploymentChainIds);
-
-  const parsed = efpLslContractSchema.safeParse(slicedLslContract);
-
-  if (!parsed.success) {
-    throw new Error(
-      "Failed to decode the encoded List Storage Location contract object: \n" +
-        prettifyError(parsed.error) +
-        "\n",
-    );
-  }
-  return parsed.data;
-}
-
-/**
- * Get the list of EFP Deployment Chain IDs for the ENS Deployment Chain.
- *
- * @param ensDeploymentChain
- * @returns list of EFP deployment chain IDs
- */
-export function getEFPDeploymentChainIds(ensDeploymentChain: ENSDeploymentChain): bigint[] {
-  switch (ensDeploymentChain) {
-    case "mainnet":
-      return [BigInt(base.id), BigInt(optimism.id), BigInt(mainnet.id)];
-    case "sepolia":
-      return [BigInt(baseSepolia.id), BigInt(optimismSepolia.id), BigInt(sepolia.id)];
-    default:
-      throw new Error(
-        `EFP Deployment chainIds are not configured for the ${ensDeploymentChain} ENS Deployment Chain`,
-      );
-  }
-}
 
 /**
  * Data structure helpful for parsing an EncodedLsl.
@@ -168,6 +199,54 @@ const createEfpLslContractSchema = (efpChainIds: bigint[]) =>
       .transform((v) => BigInt("0x" + v)),
   });
 
+// NOTE: based on code from https://github.com/ethereumfollowprotocol/onchain/blob/f3c970e/src/efp.ts#L95-L123
+/**
+ * Decodes an EncodedLsl into a ListStorageLocationContract.
+ *
+ * @param {ENSDeploymentChain} ensDeploymentChain - The ENS Deployment Chain
+ * @param {EncodedLsl} encodedLsl - The encoded List Storage Location string to parse.
+ * @returns A decoded {@link ListStorageLocationContract} object.
+ * @throws An error if parsing could not be completed successfully.
+ */
+export function decodeListStorageLocationContract(
+  ensDeploymentChain: ENSDeploymentChain,
+  encodedLsl: EncodedLsl,
+): ListStorageLocationContract {
+  const slicedLslContract = sliceEncodedLslContract(encodedLsl);
+  const efpDeploymentChainIds = getEFPDeploymentChainIds(ensDeploymentChain);
+  const efpLslContractSchema = createEfpLslContractSchema(efpDeploymentChainIds);
+
+  const parsed = efpLslContractSchema.safeParse(slicedLslContract);
+
+  if (!parsed.success) {
+    throw new Error(
+      "Failed to decode the encoded List Storage Location contract object: \n" +
+        prettifyError(parsed.error) +
+        "\n",
+    );
+  }
+  return parsed.data;
+}
+
+/**
+ * Get the list of EFP Deployment Chain IDs for the ENS Deployment Chain.
+ *
+ * @param ensDeploymentChain
+ * @returns list of EFP deployment chain IDs
+ */
+export function getEFPDeploymentChainIds(ensDeploymentChain: ENSDeploymentChain): bigint[] {
+  switch (ensDeploymentChain) {
+    case "mainnet":
+      return [BigInt(base.id), BigInt(optimism.id), BigInt(mainnet.id)];
+    case "sepolia":
+      return [BigInt(baseSepolia.id), BigInt(optimismSepolia.id), BigInt(sepolia.id)];
+    default:
+      throw new Error(
+        `EFP Deployment chainIds are not configured for the ${ensDeploymentChain} ENS Deployment Chain`,
+      );
+  }
+}
+
 /**
  * Unique EFP List Storage Location ID
  *
@@ -196,82 +275,3 @@ export const makeListStorageLocationId = ({
     listRecordsAddress.toLowerCase(),
     slot.toString(),
   ].join("-");
-
-/**
- * Base List Storage Location
- */
-interface BaseListStorageLocation<
-  LSLVersion extends ListStorageLocationVersion,
-  LSLType extends ListStorageLocationType,
-> {
-  /**
-   * The version of the List Storage Location.
-   *
-   * This is used to ensure compatibility and facilitate future upgrades.
-   */
-  version: LSLVersion;
-
-  /**
-   * The type of the List Storage Location.
-   *
-   * This identifies the kind of data the data field contains.
-   */
-  type: LSLType;
-}
-
-/**
- * List Storage Location Contract
- *
- * Describes data model for the EVM contract Location Type as
- * a specialized version of BaseListStorageLocation interface,
- * where the location type is always 1, and, for now, the version is always 1.
- *
- * Documented based on https://docs.efp.app/design/list-storage-location/
- */
-export interface ListStorageLocationContract
-  extends BaseListStorageLocation<
-    ListStorageLocationVersion.V1,
-    ListStorageLocationType.EVMContract
-  > {
-  /**
-   * EVM chain ID of the chain where the EFP list records are stored.
-   */
-  chainId: bigint;
-
-  /**
-   * EVM address of the contract on chainId where the EFP list records are stored.
-   */
-  listRecordsAddress: Address;
-
-  /**
-   * The 32-byte value that specifies the storage slot of the EFP list records within the listRecordsAddress contract.
-   * This disambiguates multiple lists stored within the same contract and
-   * de-couples it from the EFP List NFT token id which is stored on Ethereum and
-   * inaccessible on L2s.
-   */
-  slot: bigint;
-}
-
-/**
- * Enum defining recognized List Storage Location Types
- *
- * Based on documentation at:
- * https://docs.efp.app/design/list-storage-location/#location-types
- */
-export enum ListStorageLocationType {
-  /**
-   * EVMContract Data List Storage Location Type encoding:
-   * 32-byte chain ID + 20-byte contract address + 32-byte slot
-   */
-  EVMContract = 1,
-}
-
-/**
- * Enum defining recognized List Storage Location Versions
- *
- * Based on documentation at:
- * https://docs.efp.app/design/list-storage-location/#serialization
- */
-export enum ListStorageLocationVersion {
-  V1 = 1,
-}
