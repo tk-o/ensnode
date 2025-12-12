784e017db6eb7ba48a98655d4dced1e8beb400ad
Tomasz Kopacki <tomasz@kopacki.net>
2025-12-12 10:52:12 +0100

    fix(ensapi): cache RPC clients per chain ID

--- apps/ensapi/src/lib/rpc/public-client.ts
+++ apps/ensapi/src/lib/rpc/public-client.ts
@@ -4,6 +4,13 @@ import { createPublicClient, fallback, http, type PublicClient } from "viem";
 
 import type { ChainId } from "@ensnode/ensnode-sdk";
 
+const _cache = new Map<ChainId, PublicClient>();
+
+/**
+ * Gets a viem#PublicClient for the specified `chainId` using the ENSApiConfig's RPCConfig. Caches
+ * the instance itself to minimize unnecessary allocations and reduce the net_listening queries that
+ * power Transport Ranking.
+ */
 export function getPublicClient(chainId: ChainId): PublicClient {
   // Invariant: ENSApi must have an rpcConfig for the requested `chainId`
   const rpcConfig = config.rpcConfigs.get(chainId);
@@ -11,17 +18,29 @@ export function getPublicClient(chainId: ChainId): PublicClient {
     throw new Error(`Invariant: ENSApi does not have an RPC to chain id '${chainId}'.`);
   }
 
-  // Create an un-cached publicClient that uses a fallback() transport with all specified HTTP RPCs
-  return createPublicClient({
-    transport: fallback(
-      rpcConfig.httpRPCs.map((url) => http(url.toString())),
-      {
-        // Transport Ranking enables each of the Transports passed to
-        // the "fallback transport" to be automatically ranked based on their
-        // latency & stability via a weighted moving score algorithm.
-        // https://v1.viem.sh/docs/clients/transports/fallback.html#transport-ranking
-        rank: true,
-      },
-    ),
-  });
+  if (!_cache.has(chainId)) {
+    _cache.set(
+      chainId,
+      // Create an viem#PublicClient that uses a fallback() transport with all specified HTTP RPCs
+      createPublicClient({
+        transport: fallback(
+          rpcConfig.httpRPCs.map((url) => http(url.toString())),
+          {
+            // Transport Ranking enables each of the Transports passed to
+            // the Fallback Transport to be automatically ranked based on their
+            // latency & stability via a weighted moving score algorithm.
+            // https://v1.viem.sh/docs/clients/transports/fallback.html#transport-ranking
+            rank: { interval: 60_000, sampleCount: 5 },
+          },
+        ),
+      }),
+    );
+  }
+
+  const publicClient = _cache.get(chainId);
+
+  // publicClient guaranteed to exist due to cache-setting logic above
+  if (!publicClient) throw new Error("never");
+
+  return publicClient;
 }
